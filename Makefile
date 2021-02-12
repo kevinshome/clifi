ab807683474e4e1f50b85c8d123b6b5c30ff6c6f
kevinshome <noah.tanner7@gmail.com>
2021-02-12 15:51:58 -0500

    added required file/directory checks

---
+++ Makefile
@@ -0,0 +1,3 @@
+clifi_dir:
+	mkdir ${HOME}/.clifi
+	cp -r default ${HOME}/.clifi/defaults
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,43 @@ use subprocess::{Popen, PopenConfig, Redirection};
 use toml::Value;
 
 fn main() -> std::io::Result<()> {
-    // DEFINE VARIABLES FOR LATER                  
+    // DEFINE VARIABLES FOR LATER
     let mut vlc_path = "";
     let mut clifi_dir: String = "".to_string();
 
     // DEFINE CLIFI DIRECTORY
-    if cfg!(win32) { 
+    if cfg!(win32) {
         // NEEDS TO BE CHANGED
-        clifi_dir = env::var("FOO").unwrap(); 
+        clifi_dir = env::var("FOO").unwrap();
     } else if cfg!(unix) {
         clifi_dir = env::var("HOME").unwrap() + "/.clifi";
+    }
+
+    // DEFINE VLC PATH ON SYSTEM
+    if env::consts::OS == "linux" {
+        vlc_path = "/usr/bin/vlc";
+    } else if env::consts::OS == "macos" {
+        vlc_path = "/Applications/VLC.app/Contents/MacOS/VLC";
+    } else if env::consts::OS == "windows" {
+        vlc_path = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe";
+    }
+
+    // CHECK FOR REQUIRED DIRECTORIES/FILES
+    if !Path::new(&format!("{}", vlc_path)).exists() {
+        println!("VLC not found on your system\nclifi requires VLC to run\nIt can be found at https://www.videolan.org/vlc/");
+        process::exit(1);
+    }
+
+    if !Path::new(&format!("{}", clifi_dir)).exists() {
+        println!("clifi directory not found\nPlease run command 'make' from clifi repository\nIf repository is not available, it can be cloned from https://github.com/kevinshome/clifi/tree/rustrw");
+        process::exit(1);
+    }
+
+    if !Path::new(&format!("{}/clifi.cfg", clifi_dir)).exists() {
+        fs::copy(
+            &format!("{}/defaults/clifi.cfg", clifi_dir),
+            &format!("{}/clifi.cfg", clifi_dir),
+        )?;
     }
 
     // READ CONFIG FILE AND SET DEFAULT STREAM
@@ -59,18 +86,11 @@ fn main() -> std::io::Result<()> {
         };
     }
 
-    // DEFINE VLC PATH ON SYSTEM
-    if env::consts::OS == "linux" {
-        vlc_path = "/usr/bin/vlc";
-    } else if env::consts::OS == "macos" {
-        vlc_path = "/Applications/VLC.app/Contents/MacOS/VLC";
-    } else if env::consts::OS == "windows" {
-        vlc_path = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe";
-    }
-    
     // STREAM NAME AND STREAM URL
     let stream_name = &format!("{}", matches.value_of("stream").unwrap()).replace('\"', "");
-    let stream_url = config_data["streams"][stream_name]["url"].to_string().replace('\"', "");
+    let stream_url = config_data["streams"][stream_name]["url"]
+        .to_string()
+        .replace('\"', "");
 
     // VERBOSE PRINTS
     if matches.is_present("verbose") {
