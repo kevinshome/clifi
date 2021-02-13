e5690ec9441e9ae980eb94d4bfa85cbe9317ab55
kevinshome <noah.tanner7@gmail.com>
2021-02-12 21:12:49 -0500

    delete streams + refactor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,77 @@
 use clap::{App, Arg};
-use std::{env, fs, path::Path, process};
+use colored::*;
+use std::{env, fs, panic, path::Path, process};
 use subprocess::{Popen, PopenConfig, Redirection};
-use toml::Value;
 use toml_edit::{value, Document};
 
+fn check_for_default(data: &toml_edit::Document) {
+    /*
+    check for default stream in config file
+    */
+
+    let def = data["config"]["default_stream"].as_str().unwrap();
+
+    let result = panic::catch_unwind(|| {
+        data["streams"][def]["full_name"].as_str().unwrap();
+    });
+    if result.is_err() {
+        println!(
+            "{}{} ['{}'] {}\n{}",
+            "ERROR: ".bold().bright_red(),
+            "Your default stream".bright_red(),
+            def,
+            "cannot be found.".bright_red(),
+            "Please check your configuration file at '~/.clifi/clifi.cfg'".bright_red()
+        );
+        process::exit(1);
+    }
+}
+
+fn init_checks(vlc_path: &str, clifi_dir: &str) -> std::io::Result<()> {
+    /*
+    necessary pre-run checks and catches
+    */
+
+    // CHECK FOR VLC
+    if !Path::new(&format!("{}", vlc_path)).exists() {
+        println!(
+            "{}{}\n{}\n{}",
+            "ERROR: ".bold().bright_red(),
+            "VLC not found on your system".bright_red(),
+            "clifi requires VLC to run".bright_red(),
+            "It can be found at https://www.videolan.org/vlc/".bright_red()
+        );
+        process::exit(1);
+    }
+
+    //CHECK FOR CLIFI DIRECTORY
+    if !Path::new(&format!("{}", clifi_dir)).exists() {
+        println!(
+            "{}{}\n{}\n{}",
+            "ERROR: ".bold().bright_red(), 
+            "clifi directory not found".bright_red(), 
+            "Please run command 'make' from clifi repository".bright_red(), 
+            "If repository is not available, it can be cloned from https://github.com/kevinshome/clifi/tree/rustrw".bright_red()
+        );
+        process::exit(1);
+    }
+
+    //CHECK FOR CLIFI CONFIG FILE
+    if !Path::new(&format!("{}/clifi.cfg", clifi_dir)).exists() {
+        fs::copy(
+            &format!("{}/defaults/clifi.cfg", clifi_dir),
+            &format!("{}/clifi.cfg", clifi_dir),
+        )?;
+    }
+
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
+    /*
+    main function
+    */
+
     // DEFINE VARIABLES FOR LATER
     let mut vlc_path = "";
     let mut clifi_dir: String = "".to_string();
@@ -26,31 +93,19 @@ fn main() -> std::io::Result<()> {
         vlc_path = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe";
     }
 
-    // CHECK FOR REQUIRED DIRECTORIES/FILES
-    if !Path::new(&format!("{}", vlc_path)).exists() {
-        println!("VLC not found on your system\nclifi requires VLC to run\nIt can be found at https://www.videolan.org/vlc/");
-        process::exit(1);
-    }
+    let _init = init_checks(vlc_path, &clifi_dir);
 
-    if !Path::new(&format!("{}", clifi_dir)).exists() {
-        println!("clifi directory not found\nPlease run command 'make' from clifi repository\nIf repository is not available, it can be cloned from https://github.com/kevinshome/clifi/tree/rustrw");
-        process::exit(1);
-    }
-
-    if !Path::new(&format!("{}/clifi.cfg", clifi_dir)).exists() {
-        fs::copy(
-            &format!("{}/defaults/clifi.cfg", clifi_dir),
-            &format!("{}/clifi.cfg", clifi_dir),
-        )?;
-    }
-
-    // READ CONFIG FILE AND SET DEFAULT STREAM
+    // READ CONFIG FILE TO MUTABLE AND IMMUTABLE VARS
     let config_raw_string = format!(
         r#"{}"#,
         fs::read_to_string(format!("{}/clifi.cfg", clifi_dir)).unwrap()
     );
-    let config_data = config_raw_string.parse::<Value>().unwrap();
-    let default_stream = &config_data["config"]["default_stream"].to_string();
+    let immut_config_data = config_raw_string.parse::<Document>().expect("invalid doc"); // a workaround
+    let mut config_data = config_raw_string.parse::<Document>().expect("invalid doc");
+    let default_stream = immut_config_data["config"]["default_stream"]
+        .as_str()
+        .unwrap();
+    check_for_default(&immut_config_data);
 
     // CLAP DEFINITIONS
     let matches = App::new("clifi")
@@ -79,7 +134,7 @@ fn main() -> std::io::Result<()> {
             Arg::with_name("rm-stream")
                 .long("rm-stream")
                 .help("remove a stream from config file")
-                .takes_value(false),
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("list-streams")
@@ -97,6 +152,7 @@ fn main() -> std::io::Result<()> {
         )
         .get_matches();
 
+    // STREAMS MANIPULATION
     if matches.is_present("add-stream") {
         let mut stream_name = String::new();
         let mut stream_full_name = String::new();
@@ -109,13 +165,17 @@ fn main() -> std::io::Result<()> {
         println!("Stream URL: ");
         std::io::stdin().read_line(&mut stream_url).unwrap();
 
-        let mut doc = config_raw_string.parse::<Document>().expect("invalid doc");
-        doc["streams"][&stream_name.trim()]["url"] = value(stream_url.trim());
-        doc["streams"][&stream_name.trim()]["full-name"] = value(stream_full_name.trim());
-        let _nil = fs::write(format!("{}/clifi.cfg", clifi_dir), doc.to_string());
+        config_data["streams"][&stream_name.trim()]["url"] = value(stream_url.trim());
+        config_data["streams"][&stream_name.trim()]["full-name"] = value(stream_full_name.trim());
+        let _nil = fs::write(format!("{}/clifi.cfg", clifi_dir), config_data.to_string());
         process::exit(0);
     }
     if matches.is_present("rm-stream") {
+        config_data["streams"]
+            .as_table_mut()
+            .unwrap()
+            .remove(matches.value_of("rm-stream").unwrap());
+        let _nil = fs::write(format!("{}/clifi.cfg", clifi_dir), config_data.to_string());
         process::exit(0);
     }
     if matches.is_present("list-streams") {
@@ -126,29 +186,30 @@ fn main() -> std::io::Result<()> {
     if matches.is_present("kill") {
         match fs::remove_file(&format!("{}/clifi.lck", clifi_dir)) {
             Ok(_) => (),
-            Err(error) => panic!("error while attempting to delete lockfile. if error persists, try manually. (~/.clifi/clifi.lck)\n{}", error),
+            Err(error) => {
+                println!("{}\n[ {} ]", "error while attempting to delete lockfile.\nif error persists, try manually. ($ rm ~/.clifi/clifi.lck)".bright_red(), format!("{}", error).red());
+                process::exit(1);
+            },
         };
         match Popen::create(&["killall", "VLC"], PopenConfig::default()) {
             Ok(_) => process::exit(0),
-            Err(error) => panic!("error while attempting to kill VLC instances. if error persists, try manually.\n{}", error),
+            Err(error) => {
+                println!("{}\n[ {} ]", "error while attempting to kill VLC instances. if error persists, try manually. ($ killall VLC)".bright_red(), format!("{}", error).red());
+                process::exit(1);
+            },
         };
     }
 
     // STREAM NAME AND STREAM URL
     let stream_name = &format!("{}", matches.value_of("stream").unwrap()).replace('\"', "");
     let stream_url = config_data["streams"][stream_name]["url"]
-        .to_string()
+        .as_str()
+        .unwrap()
         .replace('\"', "");
-
-    // VERBOSE PRINTS
-    if matches.is_present("verbose") {
-        println!("RUNNING_STREAM = {}", stream_name);
-        println!("STREAM_URL = {}", stream_url);
-        //println!("JSON_DATA = {}", json_data["streams"]);
-        println!("CONFIG_DATA = {}", config_data["config"]["default_stream"]);
-        println!("CLIFI_DIR = {}", clifi_dir);
-        println!("VLC = {}", vlc_path);
-    }
+    let stream_name_full = config_data["streams"][stream_name]["full_name"]
+        .as_str()
+        .unwrap()
+        .replace('\"', "");
 
     // CHECK FOR LOCKFILE. IF EXISTS, EXIT
     if Path::new(&format!("{}/clifi.lck", clifi_dir)).exists() {
@@ -165,7 +226,10 @@ fn main() -> std::io::Result<()> {
             ..Default::default()
         },
     ) {
-        Ok(_) => fs::File::create(&format!("{}/clifi.lck", clifi_dir))?,
+        Ok(_) => {
+            fs::File::create(&format!("{}/clifi.lck", clifi_dir))?;
+            println!("Running stream {} ({})", stream_name, stream_name_full);
+        },
         Err(error) => panic!("error opening stream: {:?}", error),
     };
 
